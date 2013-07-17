9f13fa0381981efc20c304daef87fd274035e6d3
Yuancheng <yuancheng.peng@telecom-bretagne.eu>
2013-07-17 15:23:45 +0200

    The interface of the program is changed.
    * Instead of establishing benchenmark folders,
      this tool now take any file with pythran export as benchmark.
    
    How to use this program?
    
    - Copy the script pythran-replay into the pythran working space.
    - ./pythran-reply <pythran_file> -s <setup> <statements>
     -n <run_counts>
    
    * TODO: option [-r] from a commit to annother, with a certain steps
      between each two commits.

---
+++ benchmark/dprod.py
@@ -0,0 +1,5 @@
+#pythran export dprod(int list, int list)
+
+
+def dprod(l0,l1):
+    return sum([x*y for x,y in zip(l0,l1)])
--- benchmark/int_sum/__init__.py
+++
@@ -1,7 +0,0 @@
-#import numpy as np
-
-
-def make_env(n=1000):
-    #a = np.ones(n, dtype=np.int)
-    a = range(n)
-    return (a,), {}
